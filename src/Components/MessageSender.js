18db69e70c25735e3d2c66c37443f48b4a393195
Niidarku <niidarku66@gmail.com>
2020-10-12 11:59:03 +0000

    added Messagesender Component

--- src/Components/MessageSender.js
+++ src/Components/MessageSender.js
@@ -2,6 +2,11 @@ import React, {useState} from 'react'
 import { Avatar, Input } from '@material-ui/core'
 import './MessageSender.css'
 
+//imported icons from @material-ui
+import VideocamIcon from '@material-ui/icons/Videocam'
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+
 
 const MessageSender = () => {
   const [input, setInput] = useState('')
@@ -30,6 +35,22 @@ const MessageSender = () => {
               <button onClick={handleSubmit} type="submit">Hidden Submit</button>
             </form>
           </div>
+          <div className="messageSender__bottom">
+            <div className="messageSender__option">
+              <VideocamIcon style={{color: "red"}}/>
+              <h4>Live Video</h4>
+            </div>
+            <div className="messageSender__option">
+              <PhotoLibraryIcon style={{color: "green"}}/>
+              <h4>Photo/Video</h4>
+            </div>
+            <div className= "messageSender__option">
+              <InsertEmoticonIcon style={{color: 'orange'}}/>
+              <h3>Feeling/Activity</h3>
+            </div>
+          </div>
+
+
         </div>
     )
 }
