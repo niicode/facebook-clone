2057782ef8da6f2666ca09fe0a10b2b9b79239b8
Niidarku <niidarku66@gmail.com>
2020-10-13 10:28:45 +0000

    created post component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Feed from './Components/Feed'
 import Sidebar from './Components/Sidebar'
 import Header from './Components/Header'
+import Widget from './Components/Widget'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="app__body">
         <Sidebar/>
         <Feed/>
+        <Widget/>
       </div>
     </div>
   )
--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import MessageSender from "./MessageSender"
+import Post from "./Post"
 import StoryReel from "./StoryReel"
 const Feed = () => {
     return (
         <div className="feed">
           <StoryReel/>
           <MessageSender/>
-
+          <Post
+          profilePic =""
+          message = "PreMest project underway"
+          timestamp= "1602542640"
+          imgName = "imgName"
+          username="Nii Darku"
+          />
           {/* { 
             postsDatamap(entry =>{
               <Post
